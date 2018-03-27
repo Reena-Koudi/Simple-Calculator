453ba13cc42353814b11cb66966e115596c139b8
Reenakoudi <kreena92@gmail.com>
2018-03-27 23:18:03 +0100

    Simple calculator with unit testing and feature testing implemented

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,3 +1,15 @@
 class Calculator
+  def operations(operator, a, b)
 
+    if operator == "add"
+      return  a + b
+    elsif operator == "subtract"
+      return  a - b
+    elsif operator == "multiply"
+      return a * b
+    elsif operator == "divide"
+      return a / b
+    end
+
+  end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -2,4 +2,22 @@ require 'calculator'
 
 describe Calculator do
 
+  describe '#operations' do
+    it 'adds two numbers' do
+      expect(1 + 1).to eq 2
+    end
+
+    it 'subtracts two numbers' do
+      expect(3 - 2).to eq 1
+    end
+
+    it 'multiplies two numbers' do
+      expect(3 * 2).to eq 6
+    end
+
+    it 'divides two numbers' do
+      expect(6/2).to eq 3
+    end
+
+  end
 end
